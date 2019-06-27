73920ee5c5f24f0274b86b73d48e1a45d8cc502b
dzmitry-paluyan <dzmitry.paluyan@instinctools.com>
2019-06-27 17:24:41 +0300

    GQL-1: Added jsdocs.

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,22 +2,55 @@
 
 const EmployeeModel = require('../mongo/models/employee');
 
+/**
+ * Get all employees
+ *
+ * @return {Object} Promise with result
+ */
 function getEmployees() {
   return EmployeeModel.find({});
 }
 
+/**
+ * Get employee by identifier
+ *
+ * @param {Object<id>} - Employee identifier
+ *
+ * @return {Object} Promise with result
+ */
 function getEmployee({ id }) {
   return EmployeeModel.findById(id);
 }
 
+/**
+ * Create new employee
+ *
+ * @param {Object<firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages>} - Employee data
+ *
+ * @return {Object} Promise with result
+ */
 function createEmployee({ firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages }) {
   return EmployeeModel.create({ firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages });
 }
 
+/**
+ * Update employee by identifier
+ *
+ * @param {Object<id, firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages>} - Employee data
+ *
+ * @return {Object} Promise with result
+ */
 function updateEmployee({ id, firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages }) {
   return EmployeeModel.findByIdAndUpdate(id, { firstName, lastName, address, email, phoneNumber, dateOfBirth, primaryLanguage, languages }, { new: true });
 }
 
+/**
+ * Remove employee by identifier
+ *
+ * @param {Object<id>} - Employee identifier
+ *
+ * @return {Object} Promise with result
+ */
 function removeEmployee({ id }) {
   return EmployeeModel.findByIdAndRemove(id);
 }
